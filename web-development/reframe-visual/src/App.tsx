e2dc8232e8d051c73b2b6785a568294652b910e4
Shovon Ahmed <shovonahmedshuvoo@gmail.com>
2026-03-19 21:08:50 +0600

    feat: activate all 21 platform modules and connect routes

--- web-development/reframe-visual/src/App.tsx
+++ web-development/reframe-visual/src/App.tsx
@@ -2,15 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Public Marketing Pages
 import { LandingPage } from './pages/LandingPage';
+import { AboutPage } from './pages/AboutPage';
+import { ContactPage } from './pages/ContactPage';
+import { PricingPage } from './pages/PricingPage';
 import { InsightsPage } from './pages/InsightsPage';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { TermsPage } from './pages/TermsPage';
 import { FaqPage } from './pages/FaqPage';
+import { PortfolioPage } from './pages/PortfolioPage';
+import { ServicesCatalog } from './pages/ServicesCatalog';
 import { ServiceDetailPage } from './pages/ServiceDetailPage';
 import { SolutionDetailPage } from './pages/SolutionDetailPage';
-import { PortfolioPage } from './pages/PortfolioPage';
 
 // Authenticated Platform Pages
+import { LoginPage } from './pages/LoginPage';
+import { Dashboard } from './pages/Dashboard';
+import { AccountSettings } from './pages/AccountSettings';
+import { NewJobServices } from './pages/NewJobServices';
+import { NewJobUpload } from './pages/NewJobUpload';
+import { NewJobReview } from './pages/NewJobReview';
 import { JobDetail } from './pages/JobDetail';
 import { JobImageReview } from './pages/JobImageReview';
 import { JobPayment } from './pages/JobPayment';
@@ -21,12 +31,11 @@ function App() {
       <Routes>
         {/* Marketing Routes */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/services" element={<LandingPage />} />
-        <Route path="/pricing" element={<LandingPage />} />
-        <Route path="/about" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
         <Route path="/insights" element={<InsightsPage />} />
-        <Route path="/contact" element={<LandingPage />} />
-        <Route path="/login" element={<LandingPage />} />
+        <Route path="/services" element={<ServicesCatalog />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsPage />} />
         <Route path="/faq" element={<FaqPage />} />
@@ -35,14 +44,15 @@ function App() {
         <Route path="/solutions/:industry" element={<SolutionDetailPage />} />
 
         {/* Platform Routes */}
-        <Route path="/dashboard" element={<LandingPage />} />
-        <Route path="/jobs/new/services" element={<LandingPage />} />
-        <Route path="/jobs/new/upload" element={<LandingPage />} />
-        <Route path="/jobs/new/review" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/account" element={<AccountSettings />} />
+        <Route path="/jobs/new/services" element={<NewJobServices />} />
+        <Route path="/jobs/new/upload" element={<NewJobUpload />} />
+        <Route path="/jobs/new/review" element={<NewJobReview />} />
         <Route path="/jobs/:id" element={<JobDetail />} />
         <Route path="/jobs/:id/review" element={<JobImageReview />} />
         <Route path="/jobs/:id/payment" element={<JobPayment />} />
-        <Route path="/account" element={<LandingPage />} />
       </Routes>
     </Router>
   );
